5dbb6be9cb3d090492a672b15d1f4c6550dccdc2
aryan230 <aryan23062001@gmail.com>
2024-02-16 10:18:47 +0530

    cs22

--- src/Components/Modals/Sample/CustomSamples/CustomFeildTeplate.jsx
+++ src/Components/Modals/Sample/CustomSamples/CustomFeildTeplate.jsx
@@ -381,7 +381,7 @@ function CustomFeildTeplate({
                     type="text"
                     id="first_name"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                    placeholder="Placehodler"
+                    placeholder="Placeholder"
                     required=""
                     onChange={(e) => {
                       setPlaceholder(e.target.value);
